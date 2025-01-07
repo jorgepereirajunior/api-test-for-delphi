4eb1e91a2800395007dab757b8b501e701566e01
jorginho <jorgejunior.jpj2016@gmail.com>
2025-01-07 14:07:25 -0300

    🗃️ database: Modificacoes para testar conexao com mais de um banco de dados simultaneamente (Typeorm multiple datasource )

--- src/infra/database/config/data-source-options.ts
+++ src/infra/database/config/data-source-options.ts
@@ -4,11 +4,11 @@ import * as Settings from '@server/settings'
 
 export const dataSourceOption: DataSourceOptions = {
   type: 'mysql',
-  host: Settings.DB_HOST,
-  port: Settings.DB_PORT,
-  database: Settings.DB_NAME,
-  username: Settings.DB_USER,
-  password: Settings.DB_PASS,
+  host: Settings.DB_WEB_HOST,
+  port: Settings.DB_WEB_PORT,
+  database: Settings.DB_WEB_NAME,
+  username: Settings.DB_WEB_USER,
+  password: Settings.DB_WEB_PASS,
   logging: Settings.DB_LOGGING,
   entities: [Settings.DB_PATH_ENTITIES],
   migrations: [Settings.DB_PATH_MIGRATIONS],
--- src/server/settings/database.ts
+++ src/server/settings/database.ts
@@ -5,15 +5,15 @@ import { resolve } from 'path'
 
 const dir = resolve(__dirname, '..', '..')
 
-export const DB_HOST = process.env.DB_HOST
+export const DB_WEB_HOST = process.env.DB_WEB_HOST
 
-export const DB_PORT = +process.env.DB_PORT
+export const DB_WEB_PORT = +process.env.DB_WEB_PORT
 
-export const DB_NAME = process.env.DB_NAME
+export const DB_WEB_NAME = process.env.DB_WEB_NAME
 
-export const DB_USER = process.env.DB_USER
+export const DB_WEB_USER = process.env.DB_WEB_USER
 
-export const DB_PASS = process.env.DB_PASS
+export const DB_WEB_PASS = process.env.DB_WEB_PASS
 
 export const DB_LOGGING: LoggerOptions = ['error', 'warn']
 
